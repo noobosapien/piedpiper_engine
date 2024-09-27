fda074fc5f03485f8e239d276a1fed6032a84c77
Noobosapien <migirulz44@gmail.com>
2024-09-27 21:31:05 +1200

    Agent manager asyncio init

--- src/core/agent_manager.py
+++ src/core/agent_manager.py
@@ -1,18 +1,22 @@
+import asyncio
+
 from typing import Dict, List
 
 from .client_queue import ClientQueue
 from .agent import Agent
+from .agent_manager_thread import AgentManagerThread
+from .agent_processor import AgentProcessor
 
 
 class AgentManager:
-    _capacity_full = False
     _client_to_agent: Dict[str, List[Agent]] = {}
 
     def __init__(self):
-        pass
-
-    def at_capacity(self):
-        return self._capacity_full
+        self.loop = asyncio.new_event_loop()
+        self.agent_manager_thread = AgentManagerThread(self.loop)
+        self.agent_manager_thread.start()
+        self.agent_processor = AgentProcessor(self.loop)
+        self.agent_processor.start()
 
     def to_process(self, client_queue: ClientQueue):
         pass
---
+++ src/core/agent_processor.py
@@ -0,0 +1,6 @@
+class AgentProcessor:
+    def __init__(self, loop):
+        self._loop = loop
+
+    def start(self):
+        pass
